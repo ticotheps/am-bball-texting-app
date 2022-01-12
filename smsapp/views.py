b4451522f0145a76a7ef235850afe347b56bf578
Tico S. Thepsourinthone <ticotheps@gmail.com>
2022-01-12 00:15:18 -0500

    feat(views): define a function that can broadcast a text message for the "broadcast_sms" view

--- smsapp/views.py
+++ smsapp/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.http import HttpResponse
+from twilio.rest import Client
 
-# Create your views here.
+
+def broadcast_sms(request):
+    message_to_broadcast = ("Would you like to play basketball at the Legacy Center tomorrow at 6am? Reply '0' for NO or '1' for YES.")
+    
+    client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN, )
+    
+    for recipient in settings.SMS_BROADCAST_TO_NUMBERS:
+        if recipient:
+            client.messages.create(to=recipient, from=settings.TWILIO_NUMBER, body=message_to_broadcast)
+            
+    return HttpResponse("Messages sent!", 200)
+
+
